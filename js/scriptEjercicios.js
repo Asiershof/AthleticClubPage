32908dcc5e529118fb3d9666663f18e74aa3433e
Asiershof <asiershof@gmail.com>
2024-05-08 09:51:46 +0200

    Cambios

--- js/scriptEjercicios.js
+++ js/scriptEjercicios.js
@@ -2,12 +2,12 @@ function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("contenido");
     for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
+        tabcontent[i].style.display = "none";
     }
     tablinks = document.getElementsByClassName("pestania");
     for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    document.getElementById(tabName).style.display = "block";
+    document.getElementById(tabName).style.display = "flex";
     evt.currentTarget.className += " active";
-  }
+}
