eb57f328aa417895bedeab3d8b5e7499d8cd876d
Asiershof <asiershof@gmail.com>
2024-05-29 22:01:55 +0200

    Version 1.00

--- js/script.js
+++ js/script.js
@@ -1,11 +1,43 @@
-document.getElementById('sanmames').addEventListener('mouseover', function() {
-    cambiarImagen('imagenes/sanmamesfuera.jpg');
-});
+document.addEventListener('DOMContentLoaded', function() {
+    const imagenSanMames = document.getElementById('sanmames');
+    const imagenOriginal = 'imagenes/sanmames.jpg';
+    const imagenAlternativa = 'imagenes/sanmamesfuera.jpg';
+    let mostrandoAlternativa = false;
 
-document.getElementById('sanmames').addEventListener('mouseout', function() {
-    cambiarImagen('imagenes/sanmames.jpg');
-});
+    function cambiarImagen(nuevaImagen) {
+        imagenSanMames.src = nuevaImagen;
+    }
 
-function cambiarImagen(nuevaImagen) {
-    document.getElementById('sanmames').src = nuevaImagen;
-}
+    function configurarEventos() {
+        if (window.matchMedia("(max-width: 1024px)").matches) {
+            imagenSanMames.removeEventListener('mouseover', onMouseOver);
+            imagenSanMames.removeEventListener('mouseout', onMouseOut);
+            imagenSanMames.addEventListener('click', onClick);
+        } else {
+            imagenSanMames.removeEventListener('click', onClick);
+            imagenSanMames.addEventListener('mouseover', onMouseOver);
+            imagenSanMames.addEventListener('mouseout', onMouseOut);
+        }
+    }
+
+    function onMouseOver() {
+        cambiarImagen(imagenAlternativa);
+    }
+
+    function onMouseOut() {
+        cambiarImagen(imagenOriginal);
+    }
+
+    function onClick() {
+        if (mostrandoAlternativa) {
+            cambiarImagen(imagenOriginal);
+        } else {
+            cambiarImagen(imagenAlternativa);
+        }
+        mostrandoAlternativa = !mostrandoAlternativa;
+    }
+
+    configurarEventos();
+
+    window.addEventListener('resize', configurarEventos);
+});
